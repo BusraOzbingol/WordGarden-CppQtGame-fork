ab40e62c1a43383d7c97fb110972717040a862a6
ezgirrana <153096559+ezgirrana@users.noreply.github.com>
2025-12-16 23:45:42 +0300

    Add base CategoryEnum definition.

---
+++ CategoryEnum.h
@@ -0,0 +1,16 @@
+#ifndef CATEGORYENUM_H
+#define CATEGORYENUM_H
+
+// Sýnýf kapsamlý enum'lar (enum class), daha güvenli ve modern C++ kullanýmý saðlar.
+enum class CategoryEnum {
+    
+    Animals,        
+    Plants,         
+    Objects,        
+    Cities,         
+    Jobs,           
+    Count,          
+    Unspecified     
+};
+
+#endif // CATEGORYENUM_H
