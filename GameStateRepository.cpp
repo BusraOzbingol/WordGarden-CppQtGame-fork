f0060549aa3cb3cdf7865d58204bac8b2a0d6869
ezgirrana <153096559+ezgirrana@users.noreply.github.com>
2025-12-19 16:55:38 +0300

    Add files via upload

---
+++ GameStateRepository.cpp
@@ -0,0 +1,24 @@
+GameStateRepository::GameStateRepository() {
+    
+}
+void GameStateRepository::saveGameState(GameState* gameState) {
+    if (gameState != nullptr) {
+        // Yeni GameState nesnesini listeye ekle
+        m_gameStates.append(gameState);
+        
+        // Burasý, Qt ile dosya I/O yapacaðýnýz yerdir. 
+        // Örn: gameState'i JSON'a çevirip diske yazma (þimdilik simülasyon)
+        qDebug() << "Oyun durumu baþarýyla kaydedildi. Toplam kayýt: " << m_gameStates.size();
+    }
+}
+GameState* GameStateRepository::getLastGameState() const {
+    if (m_gameStates.isEmpty()) {
+        return nullptr; // Kayýtlý oyun durumu yok
+    }
+    
+    // Listenin son elemaný en son kaydedilen durumdur
+    return m_gameStates.last();
+}
+QList<GameState*> GameStateRepository::getAllGameStates() const {
+    return m_gameStates;
+}
---
+++ GameStateRepository.h
@@ -0,0 +1,16 @@
+#include "GameState.h"
+#include <QList>
+
+class GameStateRepository {
+public:
+    // Yapýcý metot (Eðer dosya yükleme gerekiyorsa burada yapýlýr)
+    GameStateRepository();
+
+    // Depolama Metotlarý
+    void saveGameState(GameState* gameState);
+    GameState* getLastGameState() const;
+    QList<GameState*> getAllGameStates() const;
+
+private:
+    QList<GameState*> m_gameStates; // Kaydedilen oyun durumlarý listesi
+};
