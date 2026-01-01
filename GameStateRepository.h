fb04c7669bc1857cb2b54744fb56843cb228653e
ezgirrana <ezgiranayavuz@gmail.com>
2026-01-01 13:32:15 +0300

    add Doxygen comments to GameStateRepository.h

--- GameStateRepository.h
+++ GameStateRepository.h
@@ -1,20 +1,51 @@
+/**
+ * @file GameStateRepository.h
+ * @brief Header file for the GameStateRepository class.
+ * * Defines the storage mechanism for game sessions, allowing for saving 
+ * and retrieving GameState objects.
+ */
+
 #ifndef GAMESTATEREPOSITORY_H
 #define GAMESTATEREPOSITORY_H
 
 #include "GameState.h"
 #include <QList>
 
+
+/**
+ * @class GameStateRepository
+ * @brief Manages a collection of GameState objects.
+ * * This repository acts as a data store for game sessions, providing methods 
+ * to persist the current state and retrieve historical game data.
+ */
 class GameStateRepository {
 public:
-    // Yapýcý metot (Eðer dosya yükleme gerekiyorsa burada yapýlýr)
+    /**
+	 * @brief Constructs a new GameStateRepository object.
+     * * Initialized to manage an empty list of game states.
+	  */
     GameStateRepository();
 
-    // Depolama Metotlarý
+    /**
+	  * @brief Adds a GameState to the repository.
+	  * @param gameState Pointer to the GameState object to be stored.
+	  */
     void saveGameState(GameState* gameState);
+    
+    /**
+	  * @brief Retrieves the most recently added GameState.
+	  * @return Pointer to the last GameState in the list, or nullptr if no states exist.
+      */
     GameState* getLastGameState() const;
+    
+    /**
+	 * @brief Retrieves all stored game states.
+	 * @return A QList containing pointers to all GameState objects in the repository.
+	  */
     QList<GameState*> getAllGameStates() const;
 
 private:
-    QList<GameState*> m_gameStates; // Kaydedilen oyun durumlarý listesi
+    QList<GameState*> m_gameStates; ///< Internal list storing pointers to game sessions.
+	};
 };
 #endif
