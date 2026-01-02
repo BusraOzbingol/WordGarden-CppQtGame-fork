43aa133cf17682348cceb29848fada87078872b9
Elif Yıldırım <151071085+E54309453474m@users.noreply.github.com>
2026-01-02 03:18:43 +0300

    Update PlayerManager.h

--- 3_Manager/PlayerManager.h
+++ 3_Manager/PlayerManager.h
@@ -3,16 +3,40 @@
 
 #include "2_Repository/PlayerRepository.h"
 
+/**
+ * @class PlayerManager
+ * @brief Handles creation and retrieval of Player objects.
+ */
 class PlayerManager {
 private:
+    /**
+     * @brief Pointer to the repository that stores all players.
+     */
     PlayerRepository* repository;
 
 public:
+    /**
+     * @brief Constructs a PlayerManager with the given repository.
+     * @param repository Pointer to the PlayerRepository instance used for storing and retrieving players.
+     */
     PlayerManager(PlayerRepository* repository);
 
+    /**
+     * @brief Creates a new player and adds it to the repository.
+     * @param name Name of the player to create.
+     * @param level Player level (enum PlayerLevel).
+     * @return Player* Pointer to the newly created Player object.
+     */
     Player* createPlayer(const QString& name, PlayerLevel level);
+
+    /**
+     * @brief Retrieves a player by their name.
+     * @param name Name of the player to retrieve.
+     * @return Player* Pointer to the Player if found, otherwise nullptr.
+     */
     Player* getPlayer(const QString& name);
 };
 
 #endif // PLAYERMANAGER_H
+
 
