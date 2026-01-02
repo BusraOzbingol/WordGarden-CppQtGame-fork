66182c6eb86e8f88e8b5839d55c92b75b2f656c7
Elif Yıldırım <151071085+E54309453474m@users.noreply.github.com>
2026-01-02 03:11:14 +0300

    Update HighScoreManager.h

--- 3_Manager/HighScoreManager.h
+++ 3_Manager/HighScoreManager.h
@@ -3,14 +3,30 @@
 
 #include "2_Repository/PlayerRepository.h"
 
+/**
+ * @class HighScoreManager
+ * @brief Provides functionality for retrieving top-scoring players.
+ */
 class HighScoreManager {
 private:
     PlayerRepository* repository;
+    ///< Pointer to the PlayerRepository used for data access.
 
 public:
+    /**
+     * @brief Constructs a HighScoreManager.
+     * @param repository Pointer to a PlayerRepository instance.
+     */
     HighScoreManager(PlayerRepository* repository);
+
+    /**
+     * @brief Retrieves the topN players with the highest scores.
+     * @param topN Number of top players to return.
+     * @return QList of Player pointers sorted by descending score.
+     */
     QList<Player*> getHighScores(int topN);
 };
 
 #endif // HIGHSCOREMANAGER_H
+
 
